235de102ca13f0f44a8a9abbc57dcd88dbd2c404
gileshd <gileshd@googlemail.com>
2022-08-26 16:32:36 +0200

    Core bits of synchronous bp on gauss factor graph.
    Currently runs but does looks like it is not working properly.

--- ssm_jax/bp/gauss_factor_graph.py
+++ ssm_jax/bp/gauss_factor_graph.py
@@ -3,16 +3,63 @@ import jax.numpy as jnp
 from jax.scipy import linalg
 from typing import List, NamedTuple, Optional, Tuple
 
-from ssm_jax.bp.gauss_bp_utils import info_multiply, info_divide
+from ssm_jax.bp.gauss_bp_utils import info_multiply, info_divide, info_marginalize
 
 class CanonicalPotential(NamedTuple):
     eta: jnp.ndarray
     Lambda: jnp.ndarray
 
+def extract_canonical_potential_blocks(can_pot, idxs):
+    """
+    E.g. K = [[ 0,  1,  2,  3,  4],
+              [ 5,  6,  7,  8,  9],
+              [10, 11, 12, 13, 14],
+              [15, 16, 17, 18, 19],
+              [20, 21, 22, 23, 24]]
+        h = [0, 1, 2, 3, 4, 5]
+        idxs = [1,2]
+        gets split into:
+            K11 - [[ 6,  7],
+                   [11, 12]]
+
+            K12 - [[ 5,  8,  9],
+                   [10, 13, 14]]
+
+            K22 - [[ 0,  3,  4],
+                   [15, 18, 19],
+                   [20, 23, 24]]
+        and
+            h1 - [1, 2]
+            h2 - [0, 3, 4]
+
+    Args:
+        can_pot - a CanonicalPotential object (or similar tuple) with elements (h, K) where,
+                    K - (D x D) precision matrix.
+                    h - (D,) potential vector.
+        idxs (N,) array of indices in 1,...,D.
+    Returns:
+        (K11, K12, K22), (h1, h2) - blocks of the potential parameters where:
+            K11 (N x N) block of precision elements with row and column in `indxs`
+            K12 (N x D-N) block of precision elements with row in `indxs` but column not in `indxs`.
+            K22 (D-N x D-N) block of precision elements with neither row nor column in `indxs`.
+            h1 (N,) elements of potential vector in `indxs`.
+            h2 (D-N,) elements of potential vector not in `indxs`.
+    """
+    h, K = can_pot
+    idx_range = jnp.arange(len(h))
+    b = jnp.isin(idx_range,idxs)
+    K11 = K[b,:][:,b]
+    K12 = K[b,:][:,~b]
+    K22 = K[~b,:][:,~b]
+    h1 = h[b]
+    h2 = h[~b]
+    return (K11, K12, K22), (h1, h2)
+
 def zeros_canonical_pot(dim):
     eta = jnp.zeros(dim)
     Lambda = jnp.zeros((dim,dim))
     return CanonicalPotential(eta=eta, Lambda=Lambda)
+
 
 class GaussianVariableNode:
     def __init__(self, id: int, dim: int, prior: Optional[CanonicalPotential] = None) -> None:
@@ -20,20 +67,15 @@ class GaussianVariableNode:
         self.dim = dim
         self.adj_factors = []
         self.prior = prior if prior is not None else zeros_canonical_pot(self.dim)
-        self.belief = None
-        self.messages_to_factors = dict()
+        self.belief = jax.tree_map(lambda l: l.clone(), self.prior)
 
     def update_belief(self) -> None:
         """Update local belief estimate by taking product of all incoming messages along all edges."""
-        belief = jax.tree_map(lambda l: l.copy(), self.prior)
+        belief = jax.tree_map(lambda l: l.clone(), self.prior)
         for factor in self.adj_factors:
             message = factor.messages_to_vars[self.variableID]
             belief = info_multiply(belief, message)
         self.belief = belief
-
-    def set_messages_to_zero(self) -> None:
-        for factor in self.adj_factors:
-            self.messages_to_factors[factor.factorID] = zeros_canonical_pot(self.dim)
 
 
 class CanonicalFactor:
@@ -43,83 +85,59 @@ class CanonicalFactor:
         self.adj_vIDs = [var.variableID for var in adj_var_nodes]
         self.factor = factor
         self.dim = self.factor.Lambda.shape[0]
+        self.var_scopes = self._calculate_var_scopes()
         self.messages_to_vars = dict()
 
         assert self.dim == sum([var.dim for var in adj_var_nodes])
 
-    def set_messages_to_zero(self):
-        for var in self.adj_var_nodes:
-            self.messages_to_vars[var.variableID] = zeros_canonical_pot(var.dim)
 
     def compute_messages(self, damping: float = 0.0) -> None:
-        # TODO: Make this code vaguely comprehensible.
         """Compute all outgoing messages from the factor."""
-        messages_eta, messages_lam = [], []
+        # TODO: I think there is a problem with the initial message + damping here.
+        pot_plus_all_messages = self._absorb_var_messages()
+        for var in self.adj_var_nodes:
+            vID = var.variableID
+            marginal_pot = self._marginalise_onto_var(pot_plus_all_messages, vID)
+            # Subtract off the message sent from the variable to this factor
+            var_to_factor_message = info_divide(var.belief, self.messages_to_vars[vID])
+            new_factor_to_var_message = info_divide(marginal_pot, var_to_factor_message)
+            # Apply damping
+            damped_message = jax.tree_map(lambda x,y: damping * x + (1-damping) * y,
+                                          self.messages_to_vars[vID], new_factor_to_var_message)
+            self.messages_to_vars[vID] = damped_message
 
-        start_dim = 0
-        for v in range(len(self.adj_vIDs)):
-            eta_factor, lam_factor = self.factor.eta.clone(), self.factor.lam.clone()
-            # Take product of factor with incoming messages other than message from
-            #  the current variable.
-            start = 0
-            for var in self.adj_var_nodes:
-                if var != v:
-                    var_dofs = self.adj_var_nodes[var].dofs
-                    eta_factor = eta_factor.at[start : start + var_dofs].add(
-                        self.adj_var_nodes[var].belief.eta - self.messages[var].eta
-                    )
-                    lam_factor = lam_factor.at[start : start + var_dofs, start : start + var_dofs].add(
-                        self.adj_var_nodes[var].belief.lam - self.messages[var].lam
-                    )
+    def _set_messages_to_zero(self):
+        for var in self.adj_var_nodes:
+            self.messages_to_vars[var.variableID] = zeros_canonical_pot(var.dim)
+    
+    def _calculate_var_scopes(self):
+        var_dims = jnp.array([var.dim for var in self.adj_var_nodes])
+        var_starts = jnp.concatenate((jnp.zeros(1),jnp.cumsum(var_dims)[:-1]))
+        var_stops = var_starts + var_dims
+        var_scopes = {var_id:(int(start), int(stop))
+                      for var_id, start, stop in zip(self.adj_vIDs, var_starts, var_stops)}
+        return var_scopes
 
-                start += self.adj_var_nodes[var].dofs
-
-            # Divide up parameters of distribution
-            mess_dofs = self.adj_var_nodes[v].dofs
-            # Extract the relevant section of the joint potential vector.
-            eo = eta_factor[start_dim : start_dim + mess_dofs]
-            # Combine the rest of the joint eta vector.
-            eno = jnp.concatenate((eta_factor[:start_dim], eta_factor[start_dim + mess_dofs :]))
-
-            # Extract the relevant block of the joint precision matrix.
-            loo = lam_factor[start_dim : start_dim + mess_dofs, start_dim : start_dim + mess_dofs]
-
-            # This is the cross precision of current rest x var.
-            lnoo = jnp.concatenate(
-                (
-                    lam_factor[:start_dim, start_dim : start_dim + mess_dofs],
-                    lam_factor[start_dim + mess_dofs :, start_dim : start_dim + mess_dofs],
-                ),
-                axis=0,
+    def _absorb_var_messages(self):
+        eta, Lambda = self.factor.eta.clone(), self.factor.Lambda.clone()
+        for var in self.adj_var_nodes:
+            vID = var.variableID
+            var_start, var_stop = self.var_scopes[vID]
+            # TODO: make a function to wrap these two operations
+            #  f(pot, message, scope) -> pot_plus_message
+            eta = eta.at[var_start : var_stop].add(
+                var.belief.eta - self.messages_to_vars[vID].eta
             )
-            # This is the remaining precision block rest x rest.
-            lnono = jnp.concatenate(
-                (
-                    jnp.concatenate(
-                        (lam_factor[:start_dim, :start_dim], lam_factor[:start_dim, start_dim + mess_dofs :]), axis=1
-                    ),
-                    jnp.concatenate(
-                        (
-                            lam_factor[start_dim + mess_dofs :, :start_dim],
-                            lam_factor[start_dim + mess_dofs :, start_dim + mess_dofs :],
-                        ),
-                        axis=1,
-                    ),
-                ),
-                axis=0,
+            Lambda = Lambda.at[var_start:var_stop, var_start:var_stop].add(
+                var.belief.Lambda - self.messages_to_vars[vID].Lambda
             )
+        return CanonicalPotential(eta, Lambda)
 
-            G = linalg.solve(lnono, lnoo)
-            new_message_lam = loo - G.T @ lnoo
-            new_message_eta = eo - G.T @ eno
-            messages_eta.append((1 - damping) * new_message_eta + damping * self.messages[v].eta)
-            messages_lam.append((1 - damping) * new_message_lam + damping * self.messages[v].lam)
-            start_dim += self.adj_var_nodes[v].dofs
-
-        for v in range(len(self.adj_vIDs)):
-            # Update saved messages.
-            self.messages[v].lam = messages_lam[v]
-            self.messages[v].eta = messages_eta[v]
+    def _marginalise_onto_var(self, potential, varID):
+        var_idxs = jnp.arange(*self.var_scopes[varID])
+        (K11, K12, K22), (h1, h2) = extract_canonical_potential_blocks(potential, var_idxs)
+        K_marg, h_marg = info_marginalize(K22, K12.T, K11, h2, h1)
+        return CanonicalPotential(eta=h_marg, Lambda=K_marg)
 
 
 
@@ -137,11 +155,9 @@ class GaussianFactorGraph:
         for var in factor.adj_var_nodes:
             var.adj_factors.append(factor)
 
-    def set_messages_to_zero(self):
+    def set_messages_to_zero(self) -> None:
         for factor in self.factors:
-            factor.set_messages_to_zero()
-        for var in self.var_nodes:
-            var.set_messages_to_zero()
+            factor._set_messages_to_zero()
 
     def update_all_beliefs(self) -> None:
         for var_node in self.var_nodes:
